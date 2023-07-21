bd3ff466dc0221d7227531b3f0b3c878fbeba5cb
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2023-07-21 18:43:03 +0200

    Add first simple version of the Options class header in the io library

---
+++ libraries/io/Options.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace io {
+
+  class Options {
+    public:
+      Options(int argc, char** argv);
+
+      /** Default constructor */
+      Options()
+        : Options(1, nullptr) { }
+
+      /** Default destructor */
+      ~Options() = default;
+  }
+
+} // namespace io
