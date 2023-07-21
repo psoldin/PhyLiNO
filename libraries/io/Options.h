8075153dd40d39a4663745c913323b79fc306a7d
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2023-07-21 18:58:43 +0200

    Add missing documentation

--- libraries/io/Options.h
+++ libraries/io/Options.h
@@ -4,6 +4,11 @@ namespace io {
 
   class Options {
    public:
+    /**
+     * Constructor
+     * @param argc Command line argc
+     * @param argv Command line argv
+     */
     Options(int argc, char** argv);
 
     /** Default constructor */
