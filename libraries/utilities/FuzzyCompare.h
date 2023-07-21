b58410c52064e107e5c58060f6f424e8939ddbcb
Philipp Soldin <soldin@physik.rwth-aachen.de>
2023-07-21 18:31:33 +0200

    Add initial CMakeLists.txt file for libraries and utilities library. Implement first version of FuzzyCompare header

---
+++ libraries/utilities/FuzzyCompare.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// STL includes
+#include <type_traits>
+#include <cmath>
+
+// TODO Documentation
+
+namespace utilities {
+
+  template <typename T>
+  concept is_float_double = std::is_same_v<float, std::decay_t<T>> || std::is_same_v<double, std::decay_t<T>>;
+
+  template <is_float_double T>
+  [[nodiscard]] __attribute__((always_inline)) inline bool fuzzyIsNull(T&& value) noexcept {
+    if constexpr (std::is_same_v<typename std::decay_t<T>, double>) {
+      return std::abs(value) <= 1e-12;
+    } else {
+      return std::abs(value) <= 0.00001f;
+    }
+  }
+
+  template <is_float_double> T>
+  [[nodiscard]] __attribute__((always_inline)) inline bool fuzzyCompare(T&& a, T&& b) noexcept {
+    return fuzzyIsNull(a - b);
+  }
+
+} // namespace utilities
