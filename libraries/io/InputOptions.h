6b4b1afaf947300ae6c774a3b464b55dbcddd74e
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2023-07-21 18:56:16 +0200

    Apply coding style to InputOptions headaer

--- libraries/io/InputOptions.h
+++ libraries/io/InputOptions.h
@@ -5,21 +5,21 @@ namespace io {
   // TODO Documentation
 
   class InputOptions {
-    public:
-      /** Constructor that gets the command line arguments
-       * @param argc
-       * @param argv
-       */
-      InputOptions(int argc, char** argv);
+   public:
+    /** Constructor that gets the command line arguments
+     * @param argc
+     * @param argv
+     */
+    InputOptions(int argc, char** argv);
 
-      /** Default destructor */
-      ~InputOptions() = default;
+    /** Default destructor */
+    ~InputOptions() = default;
 
-      /** Global random seed for the number generation */
-      [[nodiscard]] int seed() const noexcept { return m_Seed; }
+    /** Global random seed for the number generation */
+    [[nodiscard]] int seed() const noexcept { return m_Seed; }
 
-    private:
-      int m_Seed;
+   private:
+    int m_Seed;
   };
 
-} // namespace io
+}  // namespace io
