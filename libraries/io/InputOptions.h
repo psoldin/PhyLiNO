cc266ce26be9950f6f7da1a2d1c5cc8c458637f8
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2023-07-21 18:47:22 +0200

    Very first simple implementation of the InputOptions class. No source file yet and non functional

---
+++ libraries/io/InputOptions.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace io {
+
+  class InputOptions {
+    public:
+      /** Constructor that gets the command line arguments
+       * @param argc
+       * @param argv
+       */
+      InputOptions(int argc, char** argv);
+
+      /** Default destructor */
+      ~InputOptions() = default;
+
+      /** Global random seed for the number generation */
+      [[nodiscard]] int seed() const noexcept { return m_Seed; }
+
+    private:
+      int m_Seed;
+  };
+
+} // namespace io
