009a23f5c88f5aedbc6fee2bcc4f149564a78a80
Philipp Soldin <soldin@physik.rwth-aachen.de>
2024-01-22 19:43:15 +0100

    Set new getter function for the input options in InputOptions

--- libraries/io/InputOptions.h
+++ libraries/io/InputOptions.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// STL includes
+#include <map>
+
 // includes
 #include "InputParameter.h"
 #include "InputPaths.h"
@@ -10,7 +13,7 @@ namespace io {
 
   /**
    * @brief Class representing input options for the program.
-   * 
+   *
    * This class provides methods to access and manipulate the input options
    * for the program. It stores information such as the command line arguments,
    * random seed, silent mode flag, and other related options.
@@ -19,7 +22,7 @@ namespace io {
    public:
     /**
      * @brief Constructor that initializes the input options.
-     * 
+     *
      * @param argc The number of command line arguments.
      * @param argv The array of command line arguments.
      */
@@ -30,30 +33,77 @@ namespace io {
 
     /**
      * @brief Get the global random seed.
-     * 
+     *
      * @return The global random seed for number generation.
      */
     [[nodiscard]] int seed() const noexcept { return m_Seed; }
 
     /**
      * @brief Check if the program should run in silent mode.
-     * 
+     *
      * @return True if the program should run in silent mode, false otherwise.
      */
     [[nodiscard]] bool silent() const noexcept { return m_Silent; }
 
+    /**
+     * @brief Check if the program should use data.
+     *
+     * @return True if the program should use data, false otherwise.
+     */
+    [[nodiscard]] bool use_data() const noexcept { return m_UseData; }
+
+    /**
+     * @brief Check if the program should use statistical errors.
+     *
+     * @return True if the program should use statistical errors, false otherwise.
+     */
+    [[nodiscard]] bool use_statistical_errors() const noexcept { return m_UseStatisticalErrors; }
+
+    /**
+     * @brief Check if the program should use systematical errors.
+     *
+     * @return True if the program should use systematical errors, false otherwise.
+     */
+    [[nodiscard]] bool use_systematical_errors() const noexcept { return m_UseSystematicalErrors; }
+
+    /**
+     * @brief Check if the program should use fake bump.
+     *
+     * @return True if the program should use fake bump, false otherwise.
+     */
+    [[nodiscard]] bool fake_bump() const noexcept { return m_FakeBump; }
+
+    /**
+     * @brief Check if the program should perform likelihood scan.
+     *
+     * @return True if the program should perform likelihood scan, false otherwise.
+     */
+    [[nodiscard]] bool likelihood_scan() const noexcept { return m_LikelihoodScan; }
+
+    /**
+     * @brief Check if the program should use sterile.
+     *
+     * @return True if the program should use sterile, false otherwise.
+     */
+    [[nodiscard]] bool use_sterile() const noexcept { return m_UseSterile; }
+
+    [[nodiscard]] const auto& input_parameter() const noexcept { return *m_InputParameter; }
+
+    [[nodiscard]] const auto& input_paths(params::dc::DetectorType type) const noexcept { return m_InputPaths.at(type); }
+
    private:
-    int  m_Seed;
-    bool m_Silent;
-    bool m_UseData;
-    bool m_UseSystematicalErrors;
-    bool m_UseStatisticalErrors;
-    bool m_FakeBump;
-    bool m_LikelihoodScan;
+    long m_Seed;                  /**< The global random seed. */
+    bool m_Silent;                /**< Flag indicating if the program should run in silent mode. */
+    bool m_UseData;               /**< Flag indicating if the program should use data. */
+    bool m_UseSystematicalErrors; /**< Flag indicating if the program should use systematical errors. */
+    bool m_UseStatisticalErrors;  /**< Flag indicating if the program should use statistical errors. */
+    bool m_FakeBump;              /**< Flag indicating if the program should use a fake bump. */
+    bool m_LikelihoodScan;        /**< Flag indicating if the program should perform a likelihood scan. */
+    bool m_UseSterile;            /**< Flag indicating if the program should use sterile neutrino parameters. */
 
-    std::string m_ConfigFile;
+    std::string m_ConfigFile; /**< The configuration file path. */
 
-    std::unique_ptr<InputParameter> m_InputParameter;
-    std::vector<InputPaths>         m_InputPaths;
+    std::unique_ptr<InputParameter>                m_InputParameter; /**< The input parameter object. */
+    std::map<params::dc::DetectorType, InputPaths> m_InputPaths;     /**< The input paths map. */
   };
 }  // namespace io
