2e6a2803a4067ffc540bb82cf3c41078f42a7033
Philipp Soldin <soldin@physik.rwth-aachen.de>
2024-01-22 13:56:04 +0100

    Add documentation to the Options header

--- libraries/io/Options.h
+++ libraries/io/Options.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#include "DataBase.h"
+#include "InputOptions.h"
+
 namespace io {
 
+  /**
+   * Main class for the options that are handled in the Likelihood fit.
+   * Here the command line arguments as well as the measurement data are accessible.
+   * 
+  */
   class Options {
    public:
     /**
@@ -17,6 +25,31 @@ namespace io {
 
     /** Default destructor */
     ~Options() = default;
+
+    /**
+     * @brief Get the input options.
+     *
+     * This function returns a reference to the `InputOptions` object containing the input options.
+     *
+     * @return A const reference to the `InputOptions` object.
+     */
+    [[nodiscard]] const InputOptions& inputOptions() const noexcept { return m_InputOptions; }
+
+    /**
+     * @brief Returns a reference to the underlying database.
+     *
+     * This function returns a const reference to the `DataBase` object associated with the `Options` class.
+     * The `DataBase` object contains the data used by the `Options` class for its operations.
+     *
+     * @return A const reference to the `DataBase` object.
+     */
+    [[nodiscard]] const DataBase& dataBase() const noexcept { return m_DataBase; }
+
+  private:
+    InputOptions m_InputOptions;
+    DataBase     m_DataBase;
   };
+
+  
 
 }  // namespace io
