20fcf177ed7720faf9d4c40a1cde5b9ee82758fd
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-09 16:31:39 +0200

    Use newly implemented reset_parameter in ParameterWrapper constructor

--- libraries/likelihood/ParameterWrapper.cpp
+++ libraries/likelihood/ParameterWrapper.cpp
@@ -2,10 +2,8 @@
 
 namespace ana {
 
-  ParameterWrapper::ParameterWrapper(const double* parameter)
-    : m_RawParameter(parameter) {
-    std::copy(m_RawParameter, m_RawParameter + params::number_of_parameters(), m_UnifiedParameters.begin());
-    unify_parameters();
+  ParameterWrapper::ParameterWrapper(const double* parameter) {
+      reset_parameter(parameter);
   }
 
   void ParameterWrapper::reset_parameter(const double* parameter) {
