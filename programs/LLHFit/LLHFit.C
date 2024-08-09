0e82c6ba67c3c1af05d79e7f524accb8590dc5ab
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-09 16:56:27 +0200

    Replace object with pointer in main function for the io::Options object

--- programs/LLHFit/LLHFit.C
+++ programs/LLHFit/LLHFit.C
@@ -5,12 +5,11 @@
 #include "Fitter.h"
 
 int main(int argc, char** argv) {
-
-  io::Options options(argc, argv);
+  auto options = std::make_shared<io::Options>(argc, argv);
 
   ana::Fitter fit(options);
 
-  std::cout << options.inputOptions().seed() << std::endl;
+  std::cout << options->inputOptions().seed() << std::endl;
 
   std::cout << "Hello World!" << std::endl;
 }
