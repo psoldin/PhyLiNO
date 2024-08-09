aac0fc6c74732495af945644b95e7e71aa90769e
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-09 16:43:01 +0200

    ADd Oscillator class to CMakeLists

--- libraries/likelihood/Oscillator.cpp
+++ libraries/likelihood/Oscillator.cpp
@@ -4,12 +4,6 @@ namespace ana {
 
   Oscillator::Oscillator(const io::Options& options)
     : m_Options(options) {
-    using enum parms::dc::DetectorType;
-
-    for (auto detector : {FDI, FDII, ND}) {
-      // Get the reactor data for the detector
-      const auto& reactor_data = m_Options.database().reactor_data(detector);
-    }
   }
 
   void Oscillator::recalculate_spectra() {
