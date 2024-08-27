eff93712ccba5ccd3a7266efcc8e90fa7a490769
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-27 15:55:16 +0200

    Add Calculate_Spectrum.h, the extracted calculation of the spectrum in the background and signal shape

---
+++ libraries/likelihood/DoubleChooz/Calculate_Spectrum.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "../Definitions.h"
+
+// Eigen include
+#include <Eigen/Core>
+#include <Eigen/Eigenvalues>
+
+// STL include
+#include <span>
+
+namespace ana::dc {
+
+  namespace {
+    template <typename T>
+    inline auto pow_2(T&& t) noexcept { return t * t; }
+
+    template <int Nbins>
+    auto make_spectrum(std::span<const double> shape) noexcept {
+      Eigen::Map<Eigen::Vector<double, Nbins>>(shape.data(), Nbins);
+    }
+  }
+
+  template <int Nbins>
+  void calculate_spectrum(double rate,
+                          const Eigen::Vector<double, Nbins>& shape,
+                          std::span<const double> shape_parameter,
+                          const Eigen::Matrix<double, Nbins, Nbins>& covMatrix,
+                          return_t& result) {
+
+    double rate2 = pow_2(rate);
+
+    auto backgroundSpectrum = make_spectrum<Nbins>(shape);
+
+    auto backgroundSpectrumSubset = backgroundSpectrum.template head<Nbins>();
+    auto backgroundOuterProduct = backgroundSpectrumSubset * backgroundSpectrumSubset.transpose();
+
+    // Element-wise multiplication of the fractional covariance matrix with the outer product
+    auto defracCovMatrix = pow_2(rate) * covMatrix.cwiseProduct(backgroundOuterProduct);
+
+    using matrix_t = Eigen::Matrix<double, Nbins, Nbins>;
+
+    Eigen::SelfAdjointEigenSolver<matrix_t> eigen_solver(0.5 * (defracCovMatrix + defracCovMatrix.transpose()));
+
+    // The correction value for the eigenvalue that is iteratively increased until success
+    double eigenvalueCorrection = 4e-14;
+
+    auto compute_corrected_eigenvalues = [&eigen_solver](double correction) noexcept {
+      return eigen_solver.eigenvalues().unaryExpr([correction](double v) { return std::max(v, correction); }).asDiagonal();
+    };
+
+    const auto& eigenvectors = eigen_solver.eigenvectors();
+    matrix_t corrected_matrix;
+    Eigen::LLT<matrix_t> llt_solver;
+
+    while (true) {
+      corrected_matrix = eigenvectors * compute_corrected_eigenvalues(eigenvalueCorrection) * eigenvectors.transpose();
+      llt_solver.compute(corrected_matrix);
+      if (llt_solver.info() == Eigen::Success) {
+        break;
+      }
+      eigenvalueCorrection *= 1.1;
+    }
+
+    auto param_map = make_spectrum<Nbins>(shape_parameter);
+    Eigen::Vector<double, Nbins> shifts = llt_solver.matrixL() * param_map;
+
+    std::transform(backgroundSpectrum.cbegin(), backgroundSpectrum.cend(), result.begin(),
+                   [rate](double x) { return std::max(x * rate, 0.0); });
+
+    std::transform(shifts.cbegin(), shifts.cend(), result.cbegin(), result.begin(), std::plus<>());
+  }
+}
