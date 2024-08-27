4604b5eb14417a060617aa8591af34d11567e555
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-27 14:00:48 +0200

    Make the Likelihood class specific for DoubleChooz. For different experiments, this needs to be adjusted in a separate class

--- libraries/likelihood/Likelihood.cpp
+++ libraries/likelihood/Likelihood.cpp
@@ -1,20 +1,20 @@
 #include "Likelihood.h"
-#include "DCParameterWrapper.h"
+#include "DoubleChooz/DCParameterWrapper.h"
 
 #include "FuzzyCompare.h"
 
 namespace ana {
 
-  __attribute__((always_inline)) inline bool check_range(int from, int to, const DCParameterWrapper& parameter) noexcept {
+  /*
+  __attribute__((always_inline)) inline bool check_range(int from, int to, const ParameterWrapper& parameter) noexcept {
     return std::equal(parameter.cbegin() + from, parameter.cbegin() + (to + 1), parameter.raw_parameters().begin() + from, [](double a, double b) { return utilities::fuzzyCompare(a, b); });
   }
 
-  __attribute__((always_inline)) inline bool check_oscillation_parameters(const DCParameterWrapper& parameter) noexcept {
+  __attribute__((always_inline)) inline bool check_oscillation_parameters(const ParameterWrapper& parameter) noexcept {
     const bool check_t13 = utilities::fuzzyCompare(parameter[params::General::SinSqT13], parameter.raw_parameters()[params::General::SinSqT13]);
     const bool check_dm31 = utilities::fuzzyCompare(parameter[params::General::DeltaM31], parameter.raw_parameters()[params::General::DeltaM31]);
     return check_t13 & check_dm31;
   }
+   */
 
-  Likelihood::Likelihood() {
-  }
 }  // namespace ana
--- libraries/likelihood/Likelihood.h
+++ libraries/likelihood/Likelihood.h
@@ -2,40 +2,24 @@
 
 #include <Parameter.h>
 #include <span>
+#include "Options.h"
 
-namespace ana {
-
-  // TODO: Documentation missing
-  class AnalysisParameter {
-   public:
-    AnalysisParameter(const double* __restrict__ input_parameter) noexcept
-      : m_InputParameter{input_parameter, params::number_of_parameters()} {
-    }
-
-    ~AnalysisParameter() = default;
-
-    void set_input_parameter(std::span<const double> input_parameter) noexcept;
-
-    [[nodiscard]] std::span<const double> get_input_parameter() const noexcept;
-
-   private:
-    std::span<const double> m_InputParameter;
-  };
+namespace ana::dc {
 
   class Likelihood {
-   public:
-    Likelihood();
+  public:
+    explicit Likelihood(std::shared_ptr<io::Options> options)
+      : m_Options(std::move(options)) { }
 
-    double likelihood_function(const double* __restrict__ parameter) noexcept {
-      return likelihood_function(AnalysisParameter(parameter));
+    virtual ~Likelihood() = default;
+
+    [[nodiscard]] const std::shared_ptr<io::Options>& options() const noexcept { return m_Options; }
+
+    [[nodiscard]] double calculate_likelihood(const double* parameter) {
     }
 
-    double likelihood_function(const AnalysisParameter& parameter) const noexcept {
-      return 0.0;
-    }
-
-    AnalysisParameter correlate_parameters(const double* __restrict__ parameter) noexcept;
-
-    ~Likelihood() = default;
+  private:
+    std::shared_ptr<io::Options> m_Options;
   };
-}  // namespace ana
+
+}  // namespace ana::dc
