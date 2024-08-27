5bf21bf8373a86ba08c0f615d95c17aff42d0797
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-27 14:49:46 +0200

    Add Likelihood class files for the Double Chooz likelihood

--- libraries/likelihood/DoubleChooz/Likelihood.h
+++ libraries/likelihood/DoubleChooz/Likelihood.h
@@ -1,25 +1,14 @@
-#pragma once
+//
+// Created by Philipp Soldin on 27.08.24.
+//
 
-#include "Parameter.h"
-#include <span>
-#include "Options.h"
+#ifndef LIKELIHOOD_LIKELIHOOD_H
+#define LIKELIHOOD_LIKELIHOOD_H
 
-namespace ana::dc {
 
-  class Likelihood {
-  public:
-    explicit Likelihood(std::shared_ptr<io::Options> options)
-      : m_Options(std::move(options)) { }
+class Likelihood {
 
-    virtual ~Likelihood() = default;
+};
 
-    [[nodiscard]] const std::shared_ptr<io::Options>& options() const noexcept { return m_Options; }
 
-    [[nodiscard]] double calculate_likelihood(const double* parameter) {
-    }
-
-  private:
-    std::shared_ptr<io::Options> m_Options;
-  };
-
-}  // namespace ana::dc
+#endif //LIKELIHOOD_LIKELIHOOD_H
