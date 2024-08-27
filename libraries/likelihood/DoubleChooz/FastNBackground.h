6455873839afbce7efe59e1c5990a00a311f7d17
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-27 18:39:39 +0200

    Add FastNBackground class files

---
+++ libraries/likelihood/DoubleChooz/FastNBackground.h
@@ -0,0 +1,16 @@
+//
+// Created by Philipp Soldin on 27.08.24.
+//
+
+#ifndef LIKELIHOOD_FASTNBACKGROUND_H
+#define LIKELIHOOD_FASTNBACKGROUND_H
+
+
+
+class FastNBackground {
+
+};
+
+
+
+#endif //LIKELIHOOD_FASTNBACKGROUND_H
