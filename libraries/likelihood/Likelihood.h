cbf8d8928a3abd499f34cd5288a8dfeb29ac0fe3
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-27 14:47:12 +0200

    Add Likelihood.h header file

---
+++ libraries/likelihood/Likelihood.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Parameter.h"
+#include <span>
+#include "Options.h"
+
+namespace ana::dc {
+
+  class Likelihood {
+  public:
+    explicit Likelihood(std::shared_ptr<io::Options> options)
+      : m_Options(std::move(options)) { }
+
+    virtual ~Likelihood() = default;
+
+    [[nodiscard]] const std::shared_ptr<io::Options>& options() const noexcept { return m_Options; }
+
+    [[nodiscard]] virtual double calculate_likelihood(const double* parameter) = 0;
+
+  private:
+    std::shared_ptr<io::Options> m_Options;
+  };
+
+}  // namespace ana::dc
