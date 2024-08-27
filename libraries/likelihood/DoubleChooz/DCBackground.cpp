5d61fb7338f4531bd6724194175221491690d008
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-08-27 18:38:26 +0200

    Implement DCBackground class

---
+++ libraries/likelihood/DoubleChooz/DCBackground.cpp
@@ -0,0 +1,4 @@
+#include "DCBackground.h"
+
+namespace ana::dc {
+}
--- libraries/likelihood/DoubleChooz/DCBackground.h
+++ libraries/likelihood/DoubleChooz/DCBackground.h
@@ -1,23 +1,51 @@
 #pragma once
 
-#include "../BackgroundBase.h"
+#include "../Definitions.h"
+#include "AccidentalBackground.h"
+#include "LithiumBackground.h"
+#include "SpectrumBase.h"
 
 namespace ana::dc {
 
-  template <typename... Backgrounds>
-  class DCBackground : public BackgroundBase<DCBackground<Backgrounds...>> {
-   public:
-    template <typename... Args>
-    explicit DCBackground(Args&&... args)
-        : m_Backgrounds(Backgrounds(std::forward<Args>(args)...)...) { }
+  class FastNBkackground;
+  class DNCBackground;
 
-    template <typename T>
-    const T& get_background() const noexcept {
-      return std::get<T>(m_Backgrounds);
+  class DCBackground : public SpectrumBase<DCBackground> {
+    friend class SpectrumBase<DCBackground>;
+
+   public:
+    explicit DCBackground(std::shared_ptr<io::Options> options)
+      : SpectrumBase<DCBackground>(options)
+      , m_Accidental(options)
+      , m_Lithium(options) {}
+
+    ~DCBackground() = default;
+
+    void check_and_recalculate_spectrum(const ParameterWrapper& parameter) {
+      m_Accidental.check_and_recalculate_spectrum(parameter);
+      m_Lithium.check_and_recalculate_spectrum(parameter);
     }
 
-   private:
-    std::tuple<Backgrounds...> m_Backgrounds;
-  };
+    [[nodiscard]] Eigen::Array<double, 44, 1> return_spectrum(params::dc::DetectorType type) const noexcept {
+      const auto& acc = m_Accidental.return_spectrum(type);
+      const auto& lit = m_Lithium.return_spectrum(type);
 
-}
+      return acc + lit;
+    }
+
+   protected:
+    AccidentalBackground m_Accidental;
+    LithiumBackground    m_Lithium;
+    // FastNBkackground m_FastN;
+    // DNCBackground m_DNC;
+
+    return_t m_AccidentalCache;
+    return_t m_LithiumCache;
+    return_t m_FastNCache;
+    return_t m_DNCCache;
+
+    [[nodiscard]] Eigen::Array<double, 44, 1> return_spectrum(const ParameterWrapper& parameter, params::dc::DetectorType type) {
+      return {};
+    }
+  };
+}  // namespace ana::dc
