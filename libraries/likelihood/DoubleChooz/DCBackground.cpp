213bf4c9b0fede86e62dbcdc9e42b128d1db76d3
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-09-09 08:30:35 +0200

    Move function definitions from header to source file for DCBackground class

--- libraries/likelihood/DoubleChooz/DCBackground.cpp
+++ libraries/likelihood/DoubleChooz/DCBackground.cpp
@@ -1,4 +1,31 @@
 #include "DCBackground.h"
 
 namespace ana::dc {
+  DCBackground::DCBackground(std::shared_ptr<io::Options> options)
+    : SpectrumBase(options)
+    , m_Accidental(options)
+    , m_Lithium(options)
+    , m_FastN(options) {}
+
+  bool DCBackground::check_and_recalculate_spectra(const ParameterWrapper &parameter) {
+    bool has_changed = false;
+    has_changed |= m_Accidental.check_and_recalculate_spectra(parameter);
+    has_changed |= m_Lithium.check_and_recalculate_spectra(parameter);
+    has_changed |= m_FastN.check_and_recalculate_spectra(parameter);
+
+    if (has_changed) {
+      using enum params::dc::DetectorType;
+
+      for (auto detector : {ND, FDI, FDII}) {
+        m_Cache[detector] = m_Accidental.get_spectrum(detector)
+                            + m_Lithium.get_spectrum(detector)
+                            + m_FastN.get_spectrum(detector);
+      }
+    }
+    return has_changed;
+  }
+
+  const Eigen::Array<double, 44, 1> &DCBackground::get_spectrum(params::dc::DetectorType type) const noexcept {
+    return m_Cache.at(type);
+  }
 }
--- libraries/likelihood/DoubleChooz/DCBackground.h
+++ libraries/likelihood/DoubleChooz/DCBackground.h
@@ -10,35 +10,13 @@ namespace ana::dc {
 
   class DCBackground : public SpectrumBase {
    public:
-    explicit DCBackground(std::shared_ptr<io::Options> options)
-      : SpectrumBase(options)
-      , m_Accidental(options)
-      , m_Lithium(options)
-      , m_FastN(options) {}
+    explicit DCBackground(std::shared_ptr<io::Options> options);
 
     ~DCBackground() override = default;
 
-    bool check_and_recalculate_spectra(const ParameterWrapper& parameter) override {
-      bool has_changed = false;
-      has_changed |= m_Accidental.check_and_recalculate_spectra(parameter);
-      has_changed |= m_Lithium.check_and_recalculate_spectra(parameter);
-      has_changed |= m_FastN.check_and_recalculate_spectra(parameter);
+    bool check_and_recalculate_spectra(const ParameterWrapper& parameter) override;
 
-      if (has_changed) {
-        using enum params::dc::DetectorType;
-
-        for (auto detector : {ND, FDI, FDII}) {
-          m_Cache[detector] = m_Accidental.get_spectrum(detector)
-                              + m_Lithium.get_spectrum(detector)
-                              + m_FastN.get_spectrum(detector);
-        }
-      }
-      return has_changed;
-    }
-
-    [[nodiscard]] const Eigen::Array<double, 44, 1>& get_spectrum(params::dc::DetectorType type) const noexcept override {
-      return m_Cache.at(type);
-    }
+    [[nodiscard]] const Eigen::Array<double, 44, 1>& get_spectrum(params::dc::DetectorType type) const noexcept override;
 
    protected:
     AccidentalBackground m_Accidental;
