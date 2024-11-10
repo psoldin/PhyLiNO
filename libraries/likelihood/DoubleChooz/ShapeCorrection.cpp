4a4d19c32915e3c5963c93853f19022ef37f72be
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-11-10 20:13:44 +0100

    Add shape correction class

---
+++ libraries/likelihood/DoubleChooz/ShapeCorrection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Philipp Soldin on 10.11.24.
+//
+
+#include "ShapeCorrection.h"
---
+++ libraries/likelihood/DoubleChooz/ShapeCorrection.h
@@ -0,0 +1,16 @@
+//
+// Created by Philipp Soldin on 10.11.24.
+//
+
+#ifndef LIKELIHOOD_SHAPECORRECTION_H
+#define LIKELIHOOD_SHAPECORRECTION_H
+
+
+
+class ShapeCorrection {
+
+};
+
+
+
+#endif //LIKELIHOOD_SHAPECORRECTION_H
