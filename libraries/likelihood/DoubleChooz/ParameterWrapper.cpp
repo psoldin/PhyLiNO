5091c4df2ff6a4a827f7ec201c202665ef5403e4
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-12-11 18:01:07 +0100

    Refactor ParameterWrapper to use current parameters and improve change detection methods

--- libraries/likelihood/DoubleChooz/ParameterWrapper.cpp
+++ libraries/likelihood/DoubleChooz/ParameterWrapper.cpp
@@ -10,19 +10,23 @@ namespace ana::dc {
 
   void ParameterWrapper::reset_parameter(const double* parameter) {
     m_RawParameter = parameter;
-    std::copy(m_UnifiedParameters.cbegin(), m_UnifiedParameters.cend(), m_PreviousParameters.begin());
-    std::copy(parameter, parameter + params::number_of_parameters(), m_UnifiedParameters.begin());
+    std::copy(m_CurrentParameters.cbegin(), m_CurrentParameters.cend(), m_PreviousParameters.begin());
+    std::copy(parameter, parameter + params::number_of_parameters(), m_CurrentParameters.begin());
     unify_parameters();
   }
 
-  bool ParameterWrapper::parameter_changed(int idx) const noexcept {
-    return not utilities::fuzzyCompare(m_UnifiedParameters[idx], m_PreviousParameters[idx]);
+  bool ParameterWrapper::check_parameter_changed(int idx) const noexcept {
+    bool same = utilities::fuzzyCompare(m_CurrentParameters[idx], m_PreviousParameters[idx]);
+    return !same;
   }
 
-  bool ParameterWrapper::range_changed(int start, int end) const noexcept {
-    return not std::equal(m_UnifiedParameters.cbegin() + start, m_UnifiedParameters.cbegin() + end,
-               m_PreviousParameters.cbegin() + end, m_PreviousParameters.cbegin() + end,
-               [](double a, double b) { return utilities::fuzzyCompare(a, b); });
+  bool ParameterWrapper::check_parameter_changed(int from, int to) const noexcept {
+    bool same = std::equal(m_CurrentParameters.cbegin() + from,
+                           m_CurrentParameters.cbegin() + (to + 1),
+                           m_PreviousParameters.cbegin() + from,
+                           [](auto a, auto b) { return utilities::fuzzyCompare(a, b); });
+
+    return !same;
   }
 
   void ParameterWrapper::unify_parameters() {
--- libraries/likelihood/DoubleChooz/ParameterWrapper.h
+++ libraries/likelihood/DoubleChooz/ParameterWrapper.h
@@ -1,4 +1,4 @@
-#pragma once
+  #pragma once
 
 #include "Parameter.h"
 
@@ -24,8 +24,6 @@ namespace ana::dc {
      */
     explicit ParameterWrapper(const double* parameter);
 
-    ParameterWrapper();
-
     /**
      * @brief Default destructor.
      */
@@ -44,10 +42,10 @@ namespace ana::dc {
      * @param index The index of the parameter.
      * @return The value of the parameter.
      */
-    [[nodiscard]] double operator[](int index) const noexcept { return m_UnifiedParameters[index]; }
+    [[nodiscard]] double operator[](int index) const noexcept { return m_CurrentParameters[index]; }
 
     [[nodiscard]] std::span<const double> sub_range(int start, int end) const noexcept {
-      return std::span(m_UnifiedParameters).subspan(start, end - start);
+      return std::span(m_CurrentParameters).subspan(start, end - start);
     }
 
     /**
@@ -66,63 +64,82 @@ namespace ana::dc {
      *
      * @return An iterator to the beginning of the unified parameters.
      */
-    [[nodiscard]] inline auto begin() const noexcept { return m_UnifiedParameters.begin(); }
+    [[nodiscard]] inline auto begin() const noexcept { return m_CurrentParameters.begin(); }
 
     /**
      * @brief Returns an iterator to the end of the unified parameters.
      *
      * @return An iterator to the end of the unified parameters.
      */
-    [[nodiscard]] inline auto end() const noexcept { return m_UnifiedParameters.end(); }
+    [[nodiscard]] inline auto end() const noexcept { return m_CurrentParameters.end(); }
 
     /**
      * @brief Returns a reverse iterator to the beginning of the unified parameters.
      *
      * @return A reverse iterator to the beginning of the unified parameters.
      */
-    [[nodiscard]] inline auto rbegin() const noexcept { return m_UnifiedParameters.rbegin(); }
+    [[nodiscard]] inline auto rbegin() const noexcept { return m_CurrentParameters.rbegin(); }
 
     /**
      * @brief Returns a reverse iterator to the end of the unified parameters.
      *
      * @return A reverse iterator to the end of the unified parameters.
      */
-    [[nodiscard]] inline auto rend() const noexcept { return m_UnifiedParameters.rend(); }
+    [[nodiscard]] inline auto rend() const noexcept { return m_CurrentParameters.rend(); }
 
     /**
      * @brief Returns a constant iterator to the beginning of the unified parameters.
      *
      * @return A constant iterator to the beginning of the unified parameters.
      */
-    [[nodiscard]] inline auto cbegin() const noexcept { return m_UnifiedParameters.cbegin(); }
+    [[nodiscard]] inline auto cbegin() const noexcept { return m_CurrentParameters.cbegin(); }
 
     /**
      * @brief Returns a constant iterator to the end of the unified parameters.
      *
      * @return A constant iterator to the end of the unified parameters.
      */
-    [[nodiscard]] inline auto cend() const noexcept { return m_UnifiedParameters.cend(); }
+    [[nodiscard]] inline auto cend() const noexcept { return m_CurrentParameters.cend(); }
 
     /**
      * @brief Returns a constant reverse iterator to the beginning of the unified parameters.
      *
      * @return A constant reverse iterator to the beginning of the unified parameters.
      */
-    [[nodiscard]] inline auto crbegin() const noexcept { return m_UnifiedParameters.crbegin(); }
+    [[nodiscard]] inline auto crbegin() const noexcept { return m_CurrentParameters.crbegin(); }
 
     /**
      * @brief Returns a constant reverse iterator to the end of the unified parameters.
      *
      * @return A constant reverse iterator to the end of the unified parameters.
      */
-    [[nodiscard]] inline auto crend() const noexcept { return m_UnifiedParameters.crend(); }
+    [[nodiscard]] inline auto crend() const noexcept { return m_CurrentParameters.crend(); }
 
-    [[nodiscard]] bool parameter_changed(int idx) const noexcept;
+    /**
+     * @brief Checks if the parameter at the given index has changed.
+     *
+     * This function compares the parameter at the specified index in the current parameter array
+     * with the corresponding parameter in the previous parameter array.
+     *
+     * @param idx The index of the parameter to check.
+     * @return True if the parameter has changed, false otherwise.
+     */
+    [[nodiscard]] bool check_parameter_changed(int idx) const noexcept;
 
-    [[nodiscard]] bool range_changed(int start, int end) const noexcept;
+    /**
+     * @brief Checks if any parameter in the given range has changed.
+     *
+     * This function compares the parameters in the specified range in the current parameter array
+     * with the corresponding parameters in the previous parameter array.
+     *
+     * @param from The starting index of the range.
+     * @param to The ending index of the range.
+     * @return True if any parameter in the range has changed, false otherwise.
+     */
+    [[nodiscard]] bool check_parameter_changed(int from, int to) const noexcept;
 
    private:
-    std::array<double, params::number_of_parameters()> m_UnifiedParameters;   // Unified parameters array
+    std::array<double, params::number_of_parameters()> m_CurrentParameters;   // Unified parameters array
     std::array<double, params::number_of_parameters()> m_PreviousParameters;  // Previous parameter set for comparison
 
     const double* m_RawParameter;  // Pointer to the raw parameter array
