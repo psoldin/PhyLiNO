601a7ad0c0a473b780f2cc919168a10599687ac7
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-12-23 15:56:56 +0100

    Enhance AccidentalBackground class with detailed documentation for methods and functionality

--- libraries/likelihood/DoubleChooz/AccidentalBackground.h
+++ libraries/likelihood/DoubleChooz/AccidentalBackground.h
@@ -4,15 +4,48 @@
 
 namespace ana::dc {
 
+  /**
+   * @class AccidentalBackground
+   * @brief Represents the accidental background spectrum in the Double Chooz experiment.
+   *
+   * This class inherits from SpectrumBase and provides functionality to handle
+   * the accidental background spectrum. It includes methods to check and recalculate
+   * the spectra based on given parameters.
+   */
   class AccidentalBackground : public SpectrumBase {
   public:
     explicit AccidentalBackground(std::shared_ptr<io::Options> options)
       : SpectrumBase(std::move(options)) { }
 
+    /**
+     * @brief Default destructor for the AccidentalBackground class.
+     *
+     * This destructor overrides the base class destructor and is marked as default,
+     * indicating that the compiler should generate the default implementation.
+     */
     ~AccidentalBackground() override = default;
 
+    /**
+     * @brief Checks the current spectra and recalculates them if necessary.
+     *
+     * This function evaluates the given parameter and determines if the spectra
+     * need to be recalculated. If a recalculation is required, it performs the
+     * necessary computations to update the spectra.
+     *
+     * @param parameter The parameter to be checked, encapsulated in a ParameterWrapper object.
+     * @return True if the spectra were recalculated, false otherwise.
+     */
     bool check_and_recalculate_spectra(const ParameterWrapper& parameter) override;
 
+    /**
+     * @brief Retrieves the spectrum for a given detector type.
+     *
+     * This function returns a constant span of doubles representing the spectrum
+     * for the specified detector type.
+     *
+     * @param detector The type of detector for which the spectrum is requested.
+     * @return A constant span of doubles representing the spectrum for the specified detector.
+     */
     [[nodiscard]] std::span<const double> get_spectrum(params::dc::DetectorType detector) const override {
       return m_Spectra.at(detector);
     }
