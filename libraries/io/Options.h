a0e014093e8fe6e11ab65035906894bec9c857a7
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-12-28 19:39:27 +0100

    Refactor Options class to include dc::DCOptions and remove unused members

--- libraries/io/Options.h
+++ libraries/io/Options.h
@@ -3,6 +3,7 @@
 #include "DataBase.h"
 #include "InputOptions.h"
 #include "StartingParameter.h"
+#include "DCOptions.h"
 
 namespace io {
 
@@ -26,30 +27,12 @@ namespace io {
     /** Default destructor */
     ~Options() = default;
 
-    /**
-     * @brief Get the input options.
-     *
-     * This function returns a reference to the `InputOptions` object containing the input options.
-     *
-     * @return A const reference to the `InputOptions` object.
-     */
-    [[nodiscard]] const InputOptions& inputOptions() const noexcept { return m_InputOptions; }
-
-    /**
-     * @brief Returns a reference to the underlying database.
-     *
-     * This function returns a const reference to the `DataBase` object associated with the `Options` class.
-     * The `DataBase` object contains the data used by the `Options` class for its operations.
-     *
-     * @return A const reference to the `DataBase` object.
-     */
-    [[nodiscard]] const DataBase& dataBase() const noexcept { return m_DataBase; }
+    [[nodiscard]] const dc::DCOptions& dc_options() const noexcept { return m_DCOptions; }
 
     [[nodiscard]] const StartingParameter& starting_parameter() const noexcept { return m_StartingParameter; }
 
    private:
-    InputOptions m_InputOptions;
-    DataBase     m_DataBase;
+    dc::DCOptions m_DCOptions;
     StartingParameter m_StartingParameter;
   };
 
