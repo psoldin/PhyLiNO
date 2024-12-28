33aaf860db60686b33ee5ce18cf7d0e62e3b7f90
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-12-28 19:53:35 +0100

    Update include paths in Options.h to use DoubleChooz namespace

--- libraries/io/Options.h
+++ libraries/io/Options.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "DataBase.h"
+#include "DoubleChooz/DataBase.h"
 #include "InputOptions.h"
 #include "StartingParameter.h"
-#include "DCOptions.h"
+#include "DoubleChooz/DCOptions.h"
 
 namespace io {
 
