720d1a72d288b679bb572e3efd9e1a3b6e9cc663
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2024-12-28 19:42:31 +0100

    Add initial implementation of DataBase class with header and source files

---
+++ libraries/io/DataBase.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Philipp Soldin on 28.12.24.
+//
+
+#include "DataBase.h"
---
+++ libraries/io/DataBase.h
@@ -0,0 +1,16 @@
+//
+// Created by Philipp Soldin on 28.12.24.
+//
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+
+
+class DataBase {
+
+};
+
+
+
+#endif //DATABASE_H
