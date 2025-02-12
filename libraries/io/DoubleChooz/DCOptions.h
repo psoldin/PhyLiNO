c627349c33a1fb351a8d3baefa043ce5887350c6
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2025-02-12 15:28:07 +0100

    Implement DCOptions constructor

--- libraries/io/DoubleChooz/DCOptions.h
+++ libraries/io/DoubleChooz/DCOptions.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include "DataBase.h"
+
 namespace io::dc {
 
   class DCOptions {
   public:
-    DCOptions() = default;
+    explicit DCOptions(InputOptions& inputOptions) : m_DataBase(inputOptions) {}
 
     ~DCOptions() = default;
 
+    [[nodiscard]] const DataBase& dataBase() const noexcept { return m_DataBase; }
+
+  private:
+    DataBase m_DataBase;
   };
 
 } // namespace io::dc
