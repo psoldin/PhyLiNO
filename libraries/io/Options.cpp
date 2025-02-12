cda05b00827cce610b5b510ded0a6c9a2dc622b6
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2025-02-12 15:26:16 +0100

    Delete Options constructor implemenentation in source file

--- libraries/io/Options.cpp
+++ libraries/io/Options.cpp
@@ -1,11 +1,4 @@
 #include "Options.h"
 
 namespace io {
-
-  Options::Options(int argc, char** argv)
-    : m_InputOptions(argc, argv)
-    , m_DataBase(m_InputOptions)
-    , m_StartingParameter(m_InputOptions) {
-
-  }
 }  // namespace io
