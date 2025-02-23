5b2ed70dba5cb9d5f8b6466ac0c79cef1fa74d25
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2025-02-23 12:38:05 +0100

    Change virtual function interface for InputOptionBase

--- libraries/io/InputOptionBase.h
+++ libraries/io/InputOptionBase.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <boost/program_options.hpp>
+#include <boost/property_tree/ptree.hpp>
 
 namespace io {
 
@@ -13,7 +14,7 @@ namespace io {
 
     auto& options() noexcept { return m_Options; }
 
-    virtual void read(const boost::program_options::variables_map& vm) = 0;
+    virtual void read(const boost::program_options::variables_map& vm, const boost::property_tree::ptree& config) = 0;
 
    protected:
     boost::program_options::options_description m_Options;
