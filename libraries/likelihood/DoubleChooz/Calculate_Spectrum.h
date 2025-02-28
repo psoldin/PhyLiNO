f1fdf8b59cb646480934cae650c456349f7efeb2
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2025-02-28 16:10:46 +0100

    Cleanup Calculate_Spectrum.h with proper coding style

--- libraries/likelihood/DoubleChooz/Calculate_Spectrum.h
+++ libraries/likelihood/DoubleChooz/Calculate_Spectrum.h
@@ -13,44 +13,49 @@ namespace ana::dc {
 
   namespace {
     template <typename T>
-    inline auto pow_2(T&& t) noexcept { return t * t; }
-
-    template <int Nbins>
-    inline auto make_spectrum(std::span<const double> shape) noexcept {
-      return Eigen::Map<const Eigen::Vector<double, Nbins>>(shape.data(), Nbins);
+    inline auto pow_2(T&& t) noexcept {
+      return t * t;
     }
-  }
 
-  template <int NBins>
-  void calculate_spectrum(double rate,
-                          const Eigen::Vector<double, NBins>& shape,
-                          std::span<const double> shape_parameter,
-                          const Eigen::Matrix<double, NBins, NBins>& covMatrix,
-                          return_t<NBins>& result) {
+    inline auto make_spectrum(std::span<const double> shape) noexcept {
+      return Eigen::Map<const Eigen::VectorXd>(shape.data(), shape.size());
+    }
+  }  // namespace
 
-    auto backgroundSpectrum = make_spectrum<NBins>(shape);
+  inline void calculate_spectrum(double                  rate,
+                                 std::span<const double> shape,
+                                 std::span<const double> shape_parameter,
+                                 const Eigen::MatrixXd&  covMatrix,
+                                 std::span<double>       result) {
+    auto backgroundSpectrum = make_spectrum(shape);
 
-    auto backgroundSpectrumSubset = backgroundSpectrum.template head<NBins>();
+    auto backgroundSpectrumSubset = backgroundSpectrum.head(covMatrix.rows());
+
     auto backgroundOuterProduct = backgroundSpectrumSubset * backgroundSpectrumSubset.transpose();
 
     // Element-wise multiplication of the fractional covariance matrix with the outer product
     auto defracCovMatrix = pow_2(rate) * covMatrix.cwiseProduct(backgroundOuterProduct);
 
-    using matrix_t = Eigen::Matrix<double, NBins, NBins>;
+    using matrix_t = Eigen::MatrixXd;
 
     Eigen::SelfAdjointEigenSolver<matrix_t> eigen_solver(0.5 * (defracCovMatrix + defracCovMatrix.transpose()));
 
     // The correction value for the eigenvalue that is iteratively increased until success
     double eigenvalueCorrection = 4e-14;
 
+    // Function to compute the corrected eigenvalues
     auto compute_corrected_eigenvalues = [&eigen_solver](double correction) noexcept {
       return eigen_solver.eigenvalues().unaryExpr([correction](double v) { return std::max(v, correction); }).asDiagonal();
     };
 
+    // Get the eigenvectors of the rescaled covariance matrix
     const auto& eigenvectors = eigen_solver.eigenvectors();
-    matrix_t corrected_matrix;
+
+    matrix_t             corrected_matrix;
     Eigen::LLT<matrix_t> llt_solver;
 
+    // Iteratively increase the correction value until the matrix is positive definite
+    // Compute the Cholesky decomposition of the corrected matrix
     while (true) {
       corrected_matrix = eigenvectors * compute_corrected_eigenvalues(eigenvalueCorrection) * eigenvectors.transpose();
       llt_solver.compute(corrected_matrix);
@@ -60,12 +65,15 @@ namespace ana::dc {
       eigenvalueCorrection *= 1.1;
     }
 
-    auto param_map = make_spectrum<NBins>(shape_parameter);
-    Eigen::Vector<double, NBins> shifts = llt_solver.matrixL() * param_map;
+    auto param_map = make_spectrum(shape_parameter);
 
-    std::transform(backgroundSpectrum.cbegin(), backgroundSpectrum.cend(), result.begin(),
-                   [rate](double x) { return std::max(x * rate, 0.0); });
+    Eigen::VectorXd shifts = llt_solver.matrixL() * param_map;
 
-    std::transform(shifts.cbegin(), shifts.cend(), result.cbegin(), result.begin(), std::plus<>());
+    // Copy the background spectrum to the result, scaled by the rate
+    std::ranges::transform(std::as_const(backgroundSpectrum), result.begin(),
+                           [rate](double x) { return std::max(x * rate, 0.0); });
+
+    // Add the shifts to the result
+    std::transform(shifts.cbegin(), shifts.cend(), result.begin(), result.begin(), std::plus<>());
   }
-}
+}  // namespace ana::dc
