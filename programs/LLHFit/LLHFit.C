d557158d3abfa5a1d875018a91e422b560637e4d
Philipp Soldin <philipp.soldin@rwth-aachen.de>
2025-04-11 19:23:54 +0200

    Implement first running fit version (debugging only at the moment)

--- programs/LLHFit/LLHFit.C
+++ programs/LLHFit/LLHFit.C
@@ -11,19 +11,30 @@
 
 #include "DoubleChooz/DCLikelihood.h"
 
+#include "write_results.h"
+
 #include <TFile.h>
 #include <TH1D.h>
 #include <TH2D.h>
+#include <TROOT.h>
 
 #include <numeric>
 
 int main(int argc, char** argv) {
+  ROOT::EnableThreadSafety();
 
+  // try {
   auto options = std::make_shared<io::Options>(argc, argv);
 
   ana::Fit fit(options);
 
   fit.minimize();
+  result::write_results(fit, "Output");
 
   std::cout << "####\t" << fit.get_minimizer()->X()[0] << '\n';
+  // } catch (std::exception& e) {
+  //   std::cout << e.what() << '\n';
+  return EXIT_FAILURE;
+  // }
+  return EXIT_SUCCESS;
 }
